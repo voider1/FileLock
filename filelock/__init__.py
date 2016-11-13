520f20ba4a5344c0c58f85e9f904e837e5c6e6e7
Wesley Gahr <wesleyey0408@gmail.com>
2016-11-13 01:31:12 +0100

    Better error messages, added readlock functions.

--- filelock/__init__.py
+++ filelock/__init__.py
@@ -8,11 +8,12 @@ class SetFileLockError(Exception):
 
 def lock_factory():
     write_locked_files = set([])
+    read_locked_files = set([])
 
     def set_write_lock(filename):
         nonlocal write_locked_files
         if filename in write_locked_files:
-            raise SetFileLockError("File is already locked!")
+            raise SetFileLockError("File is already write locked!")
         write_locked_files.add(filename)
 
     def is_write_locked(filename):
@@ -21,11 +22,34 @@ def lock_factory():
 
     def release_write_lock(filename):
         nonlocal write_locked_files
-        write_locked_files.remove(filename)
+        try:
+            write_locked_files.remove(filename)
+        except KeyError:
+            raise ValueError("File is not write locked.")
 
-    return (set_write_lock, is_write_locked, release_write_lock)
+    def set_read_lock(filename):
+        nonlocal read_locked_files
+        if filename in read_locked_files:
+            raise SetFileLockError("File is already read locked!")
+        read_locked_files.add(filename)
+
+    def is_read_locked(filename):
+        nonlocal read_locked_files
+        return filename in read_locked_files
+
+    def release_read_lock(filename):
+        nonlocal read_locked_files
+        try:
+            read_locked_files.remove(filename)
+        except KeyError:
+            raise ValueError("File is not read locked.")
+
+    return (set_write_lock, is_write_locked, release_write_lock, set_read_lock,
+            is_read_locked, release_read_lock)
 
 
-set_write_lock, is_write_locked, release_write_lock = lock_factory()
+set_write_lock, is_write_locked, release_write_lock, set_read_lock, \
+        is_read_locked, release_read_lock = lock_factory()
 
+from filelock.readlock import ReadLock
 from filelock.writelock import WriteLock
